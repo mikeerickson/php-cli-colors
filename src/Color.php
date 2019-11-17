1c8f42ec88499f9f816488301299f9495e90c02b
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-17 13:44:22 +0100

    Replace reset-all with unbold, in foreground-colors

--- src/Color.php
+++ src/Color.php
@@ -23,28 +23,28 @@ class Color
     const HIDDEN = "\e[8m";
     const UN_HIDDEN = "\e[28m";
 
-    // Forground colors (Warning: some include reset-all)
-    const BLACK = "\033[0;30m";
+    // Forground colors (Warning: some include bold / unbold)
+    const BLACK = "\033[2;30m";
     const DARK_GRAY = "\033[1;30m";
-    const RED = "\033[0;31m";
+    const RED = "\033[2;31m";
     const LIGHT_RED = "\033[1;31m";
-    const GREEN = "\033[0;32m";
+    const GREEN = "\033[2;32m";
     const LIGHT_GREEN = "\033[1;32m";
-    const BROWN = "\033[0;33m";
-    const YELLOW = "\033[0;33m";
-    const BLUE = "\033[0;34m";
+    const BROWN = "\033[2;33m";
+    const YELLOW = "\033[2;33m";
+    const BLUE = "\033[2;34m";
     const LIGHT_BLUE = "\033[1;34m";
-    const MAGENTA = "\033[0;35m";
-    const PURPLE = "\033[0;35m";
+    const MAGENTA = "\033[2;35m";
+    const PURPLE = "\033[2;35m";
     const LIGHT_MAGENTA = "\033[1;35m";
     const LIGHT_PURPLE = "\033[1;35m";
-    const CYAN = "\033[0;36m";
+    const CYAN = "\033[2;36m";
     const LIGHT_CYAN = "\033[1;36m";
-    const LIGHT_GRAY = "\033[0;37m";
+    const LIGHT_GRAY = "\033[2;37m";
     const BOLD_WHITE = "\033[1;38m";
     const LIGHT_WHITE = "\033[1;38m";
     const FG_DEFAULT = "\033[39m";
-    const GRAY = "\033[0;90m";
+    const GRAY = "\033[2;90m";
     const LIGHT_RED_ALT = "\033[91m";
     const LIGHT_GREEN_ALT = "\033[92m";
     const LIGHT_YELLOW_ALT = "\033[93m";
